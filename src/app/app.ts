be2bea6a26d94e25c46adbf54de7deb5fc6c6c03
kamiloses <kamil.kurzaj04@gmail.com>
2025-10-31 08:02:48 +0100

    Added left-sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
+import {LeftSidebar} from './left-sidebar/left-sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, LeftSidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
