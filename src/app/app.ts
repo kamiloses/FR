e57d3ba1165816076e2e9d7edd0468728b78d44d
kamiloses <kamil.kurzaj04@gmail.com>
2025-10-31 08:07:31 +0100

    Modified right-sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {LeftSidebar} from './left-sidebar/left-sidebar';
+import {RightSidebar} from './right-sidebar/right-sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, LeftSidebar],
+  imports: [RouterOutlet, Header, LeftSidebar, RightSidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
