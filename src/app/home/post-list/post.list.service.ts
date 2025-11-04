6f457899ca1c16be55fe351ceba1a92c39633944
kamiloses <kamil.kurzaj04@gmail.com>
2025-11-04 09:49:49 +0100

    Implemented post-list-service

---
+++ src/app/home/post-list/post.list.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {PostResponseModel} from './post.response.model';
+import {Observable} from 'rxjs';
+
+@Injectable(
+  {providedIn: 'root'}
+)
+
+export class PostListService {
+//todo const a readonly
+  private readonly apiUrl = 'http://localhost:8080/api/posts';
+
+  constructor(private readonly http: HttpClient) { }
+
+
+
+  getAllPosts():Observable<PostResponseModel[]> {
+    const username = "kamilosesx"
+    return this.http.get<PostResponseModel[]>(`${this.apiUrl}?username=${username}`
+    );
+  }
+
+  sendPost(text:string):Observable<any>{
+    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel augue in purus dictum blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam dictum orci vel lorem sollicitudin, vel pharetra elit viverra. Quisque at erat ut erat feugiat dapibus. Aliquam erat volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.\n";
+    const body = { content: text };
+    return this.http.post("http://localhost:8080/api/posts/kamiloses1", body);
+  }
+
+}
